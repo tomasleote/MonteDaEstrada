67246bdd5d6e849ef301f307087693657beb49f7
tomas.leote <tomas.leote@gmail.com>
2026-07-07 21:36:23 +0100

    removed cms config and admin dahsboards

--- apps/monte-da-estrada/src/main.jsx
+++ apps/monte-da-estrada/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import { AuthProvider } from './context/AuthContext'
 import './styles/global.scss'
 import App from './App.jsx'
 
@@ -15,9 +14,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter basename={basePath}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>,
